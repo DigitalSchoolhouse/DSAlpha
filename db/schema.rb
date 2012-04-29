18de74a6bc9a3b6c7c1a4a0e52b2d7ac2b61405e
Greg Cardoni <greg@cardoni.net>
2012-04-29 18:19:13 -0500

    Added 'User' model ; Added 'user_id' column to 'Posts' model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120423000754) do
+ActiveRecord::Schema.define(:version => 20120429231535) do
 
   create_table "posts", :force => true do |t|
     t.string   "title"
@@ -20,6 +20,17 @@ ActiveRecord::Schema.define(:version => 20120423000754) do
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
     t.string   "asset_url"
+    t.integer  "user_id"
   end
+
+  create_table "users", :force => true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.boolean  "admin"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
 end
