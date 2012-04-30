d1f96587bad7a41866ffdfb87d248cbbd8649bcb
Greg Cardoni <greg@cardoni.net>
2012-04-29 20:37:10 -0500

    basic image/video embedding

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,13 @@
 module PostsHelper
+  include AutoHtml
+  
+  def asset_embed(url)
+      auto_html(url) {
+      html_escape
+      image
+      youtube(:width => 400, :height => 250)
+      link :target => "_blank", :rel => "nofollow"
+      simple_format
+      }
+  end
 end
